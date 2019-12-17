5dc56c5112605b7d6cad89b4868cf41f523cb041
Jackwchen <jackru6g6@gmail.com>
2019-12-17 16:17:13 +0800

    1、新增：MongoDB連線

---
+++ LineBotSDK/Repository/Test/TestRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace LineBotSDK.Repository.Test
+{
+    public class TestRepository
+    {
+
+        public TestRepository()
+        {
+            MongoClient test = new MongoClient();
+        }
+    }
+}
